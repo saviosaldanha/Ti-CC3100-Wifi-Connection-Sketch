60211abd2c2989460240dc82952872ef67b4f264
saviosaldanha <2712savio@gmail.com>
2016-04-02 21:01:05 +1100

    Added files via upload

---
+++ WifiConnectionexample.ino
@@ -0,0 +1,35 @@
+#include <SPI.h>
+#include <WiFi.h>
+
+char ssid[] = "catastrophy_killy";
+char password[] = "kate0101";
+void setup()
+{
+	Serial.begin(115200);
+	WiFi.begin(ssid, password);
+        // we need to start a loop to allow the connection to the Wifi Access point to occur
+	while ( WiFi.status() != WL_CONNECTED) {
+		// print dots while we wait to connect with a 50 millisecond delay loop
+		Serial.print(" . .");
+		delay(50);
+	}
+	Serial.println("The IoT device is connected, now getting an IP address for your device ");
+	Serial.println("Waiting for an ip address");
+	while (WiFi.localIP() == INADDR_NONE) {
+		// print dots while we wait for an ip addresss with a 50 millisecond delay loop
+		Serial.print(" . .");
+		delay(50);
+	}
+	Serial.println("Your IoT device has an IP address");
+	//Serial.print("IP Address: ");
+        IPAddress ip = WiFi.localIP();
+        Serial.println(ip);
+}
+
+
+
+void loop()
+{
+
+}
+
